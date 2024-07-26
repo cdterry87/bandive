25da7cbeecad3914312fe6923c458427be150d3a
Chase Terry <chase.terry87@gmail.com>
2024-07-26 01:53:57 -0500

    Attempts logo animation fix

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -6,9 +6,7 @@ const logo = Covered_By_Your_Grace({
 })
 
 const Logo = ({ isFormShown }) => {
-  let animationClasses = isFormShown
-    ? 'animate__fadeInDown'
-    : 'animate__fadeInUp'
+  let animationClasses = isFormShown ? 'animate__fadeInDown' : 'animate__fadeIn'
 
   return (
     <div>
